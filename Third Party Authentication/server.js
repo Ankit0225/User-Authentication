c1da0b745436a89e36971afa2b18d20188bc74e1
Ankit <ankitmaurya0225@gmail.com>
2021-08-29 08:22:36 +0530

    basic info

--- Third Party Authentication/server.js
+++ Third Party Authentication/server.js
@@ -1,9 +1,19 @@
 const { urlencoded } = require('express')
 const express = require('express')
+const { db } = require('./db')
 
 const app = express()
 app.set = express.json()
 
-app.listen('3000', () => {
-    console.log('Server Started on http://localhost:3000');
+app.use(express.urlencoded({extended: true}))
+db.sync()
+.then(()=> {
+
+    app.listen('3000', () => {
+        console.log('Server Started on http://localhost:3000');
+    })
+})
+
+.catch((err)=> {
+    console.log(err);
 })
